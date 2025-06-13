18d4dbc239304eed00b75426ec164dc3a59bd2fe
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-06-12 20:35:15 -0500

    added syntax and useless why3 server

--- main.ml
+++ main.ml
@@ -1,43 +1,121 @@
 open Why3
 open Format
+open Itp_server
+open Itp_communication
+open Whyconf
 
-(*This initializes a var fmla_true = true*)
-let fmla_true : Term.term = Term.t_true
+exception Badparsing
 
-(*This initializes a var fmla_false = false*)
-let fmla_false : Term.term = Term.t_false
+let print_request_json fmt (r : ide_request) =
+  (try
+     let s = Pp.string_of Json_util.print_request r in
+     let x = Json_util.parse_request s in
+     if r = x then
+       ()
+     else
+       raise Badparsing
+   with
+  | _ -> Format.eprintf "Bad parsing@.");
+  Json_util.print_request fmt r
 
-(*This initializes a var fmla_true or fmla_false, which is true \/ false*)
-let fmla1 : Term.term = Term.t_or fmla_true fmla_false
+let print_notification_json fmt (n : notification) =
+  (try
+     let x =
+       Json_util.parse_notification
+         (Pp.string_of Json_util.print_notification n)
+     in
+     if n = x then
+       ()
+     else
+       raise Badparsing
+   with
+  | _ -> Format.eprintf "Bad parsing@.");
+  Json_util.print_notification fmt n
 
-(*The type lsymbol is the type of function and predicate symbols, which we can call logic symbols*)
-(*This initializes a var prop_var_A as a Propositional var A*)
-let prop_var_A : Term.lsymbol =
-  Term.create_psymbol (Ident.id_fresh "A") []
+let debug_json =
+  Debug.register_flag "json_proto"
+    ~desc:"Print@ json@ requests@ and@ notifications@"
 
-(*This initializes a var prop_var_B as a Propositional var B*)
-let prop_var_B : Term.lsymbol =
-  Term.create_psymbol (Ident.id_fresh "B") []
+module Protocol_why3ide = struct
+  let debug_proto =
+    Debug.register_flag "ide_proto"
+      ~desc:"Print@ debugging@ messages@ about@ Why3Ide@ protocol@"
 
-(*We apply a general function for applying lsymbol to a list of terms, we just have an empty list for now*)
-let atom_A : Term.term = Term.ps_app prop_var_A []
-let atom_B : Term.term = Term.ps_app prop_var_B []
+  let print_request_debug r =
+    Debug.dprintf debug_proto "request %a@." print_request r;
+    Debug.dprintf debug_json "%a@." print_request_json r
 
-(*This makes A and B implies A*)
-let fmla2 : Term.term =
-  Term.t_implies (Term.t_and atom_A atom_B) atom_A
+  let print_notify_debug n =
+    Debug.dprintf debug_proto "handling notification %a@." print_notify n;
+    Debug.dprintf debug_json "%a@." print_notification_json n
 
-let task1 : Task.task = None (* empty task *)
-let goal_id1 : Decl.prsymbol = Decl.create_prsymbol (Ident.id_fresh "goal1") (*makes a goal*)
-let task1 : Task.task = Task.add_prop_decl task1 Decl.Pgoal goal_id1 fmla1 (*Makes a prop of type Pgoal*)
+  let list_requests : ide_request list ref = ref []
 
-let task2 = None (* task for formula 2 *)
-let task2 = Task.add_param_decl task2 prop_var_A (* Adds A as an abstract proposiitonal symbol to task2 *)
-let task2 = Task.add_param_decl task2 prop_var_B (* Adds B as an abstract proposiitonal symbol to task2 *)
-let goal_id2 = Decl.create_prsymbol (Ident.id_fresh "goal2") (* Makes a goal*)
-let task2 = Task.add_prop_decl task2 Decl.Pgoal goal_id2 fmla2 (*Makes a prop of type Pgoal*)
+  let get_requests () =
+    let n = List.length !list_requests in
+    if n > 0 then Debug.dprintf debug_proto "got %d new requests@." n;
+    let l = List.rev !list_requests in
+    list_requests := [];
+    l
 
-let () = printf "@[formula 1 is:@ %a@]@." Pretty.print_term fmla1;
-          printf "@[formula 2 is:@ %a@]@." Pretty.print_term fmla2;
-           printf "@[task 1 is:@\n%a@]@." Pretty.print_task task1;
-            printf "@[task 2 created:@\n%a@]@." Pretty.print_task task2
+  let send_request r =
+    print_request_debug r;
+    list_requests := r :: !list_requests
+
+  let notification_list : notification list ref = ref []
+
+  let notify n =
+    (* too early, print when handling notifications print_notify_debug n; *)
+    notification_list := n :: !notification_list
+
+  let get_notified () =
+    let n = List.length !notification_list in
+    if n > 0 then Debug.dprintf debug_proto "got %d new notifications@." n;
+    let l = List.rev !notification_list in
+    notification_list := [];
+    l
+end
+
+(* The gtk scheduler is catching all exceptions avoiding the printing of the
+   backtrace that is normally done by debug option stack_trace. To recover this
+   behavior we catch exceptions ourselves. If "stack_trace" is on, we exit on
+   first exception and print backtrace on standard output otherwise we raise the
+   exception again (with information on error output). *)
+let backtrace_and_exit f () =
+  try f () with
+  | e ->
+    if Debug.test_flag Debug.stack_trace then (
+      Printexc.print_backtrace stderr;
+      Format.eprintf "exception '%a' was raised in a LablGtk callback.@."
+        Exn_printer.exn_printer e;
+      exit 1
+    ) else (
+      Format.eprintf "exception '%a' was raised in a LablGtk callback.@."
+        Exn_printer.exn_printer e;
+      Format.eprintf "This should not happen. Please report. @.";
+      raise e
+    )
+
+module Scheduler = struct
+  let blocking = false
+  let multiplier = 3
+
+  let idle ~prio f =
+    let (_ : GMain.Idle.id) = GMain.Idle.add ~prio (backtrace_and_exit f) in
+    ()
+
+  let timeout ~ms f =
+    let (_ : GMain.Timeout.id) =
+      GMain.Timeout.add ~ms ~callback:(backtrace_and_exit f)
+    in
+    ()
+end
+
+module Server = Itp_server.Make (Scheduler) (Protocol_why3ide)
+
+let config = Whyconf.init_config None
+let main : Whyconf.main = Whyconf.get_main config
+let env : Env.env = Env.create_env (Whyconf.loadpath main)
+
+let () =
+  Server.init_server (read_config None) env "session_dir";

e5b74ef6a86ae0bd12291349522b2ba6bfa752c8
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-06-21 20:41:58 -0500

    added refresh, which counts the nodes

--- ftplugin/why/win.vim
+++ ftplugin/why/win.vim
@@ -14,7 +14,9 @@ command! Why3End call s:EndWhy3Session()
 command! StartServer call s:Start_Shell()
 command! StopServer call s:Stop_Shell()
 command! PrintSession call s:Print_Session()
+command! NextNode call s:Next_Node()
 command! ExitSession call s:Exit_Session()
+command! RefreshSession call s:Refresh_Session()
 
 let s:job_id = 0
 let s:regex_type = ""
@@ -26,11 +28,27 @@ function! s:OnEvent(id, data, event) abort dict
   call py3eval('why3.On_Ev(vim.eval("str"))') 
 endfunction
 
+function! s:Refresh_Session() abort
+  let running = jobwait([s:job_id], 0)[0] == -1
+  if running == 0
+    throw "Shell server is not running"
+  else 
+    " run p on shell
+    let s:regex_type = "initialize"
+    let l:out = chansend(s:job_id, "p\n")
+    if l:out == 0 
+      throw "Failed to initialize session" 
+    else 
+    endif
+  endif
+endfunction
+
 function! s:Start_Shell() abort
   let running = jobwait([s:job_id], 0)[0] == -1
   if running == 1 
     echomsg "shell server already running"
   else
+    let s:regex_type = "start"
     let s:job_id = jobstart(['./why3', 'shell', 'hello.why'], {'on_stdout': function('s:OnEvent') })
     if s:job_id == 0 
       throw "Failed to start shell server?"
@@ -38,7 +56,6 @@ function! s:Start_Shell() abort
       throw "Where is the executeable?"
     else
       echomsg "started shell with id of " . s:job_id
-      let s:regex_type = "start"
     endif
   endif
 endfunction
@@ -77,6 +94,21 @@ function! s:Print_Session() abort
       throw "Failed to print session" 
     else 
       let s:regex_type = "p"
+    endif
+  endif
+endfunction
+
+function! s:Next_Node() abort
+  let running = jobwait([s:job_id], 0)[0] == -1
+  if running == 0
+    throw "Shell server is not running"
+  else
+    " run ng on shell
+    let l:out = chansend(s:job_id, "ng\n")
+    if l:out == 0 
+      throw "Failed to move to next node" 
+    else 
+      let s:regex_type = "ng"
     endif
   endif
 endfunction
--- python/why3.py
+++ python/why3.py
@@ -105,62 +105,75 @@ def grab_selected_goal(s_str):
     else:
         raise RegexFailure("Failed to regex name in selected goal")
 
-def grab_goals(s_str):
+def grab_goals(s_str, n=None):
     goals_str = re.findall(r"{[^}]*}", s_str)
-    goals = []
-    for goal in goals_str:
-        match_name = re.search(r"Goal=(.*),", goal)
-        if match_name:
-            name = match_name.group(1)
-            match_id = re.search(r"id = (\d+)", goal)
-            if match_id:
-                id = match_id.group(1)
-                match_parent = re.search(r"parent=(.*);", goal)
-                if match_parent:
-                    parent = match_parent.group(1)
-                    goals.append(Goal(name, id, parent, []).__dict__)
+    if n is not None:
+        num_of_nodes = n + len(goals_str)
+        return num_of_nodes
+    else:
+        goals = []
+        for goal in goals_str:
+            match_name = re.search(r"Goal=(.*),", goal)
+            if match_name:
+                name = match_name.group(1)
+                match_id = re.search(r"id = (\d+)", goal)
+                if match_id:
+                    id = match_id.group(1)
+                    match_parent = re.search(r"parent=(.*);", goal)
+                    if match_parent:
+                        parent = match_parent.group(1)
+                        goals.append(Goal(name, id, parent, []).__dict__)
+                    else:
+                        raise RegexFailure("Failed to regex parent in goal")
                 else:
-                    raise RegexFailure("Failed to regex parent in goal")
+                    raise RegexFailure("Failed to regex id in goal")
             else:
-                raise RegexFailure("Failed to regex id in goal")
-        else:
-            raise RegexFailure("Failed to regex name in goal")
-    return goals
+                raise RegexFailure("Failed to regex name in goal")
+        return goals
 
-def grab_theories(s_str):
-    theories_str = re.findall(r"Theory (.*?)(?= Theory|$)", s_str)
-    theories = []
-    for theory in theories_str:
-        name_match = re.search(r"^(\S+),", theory)
-        if name_match:
-            name = name_match.group(1)
-            id_match = re.search(r"id:\s(\d+)", theory)
-            if id_match:
-                id = id_match.group(1)
-                goals = grab_goals(theory)
-                theories.append(Theory(id, name, goals).__dict__)
-                return theories 
+def grab_theories(s_str, n=None):
+    theories_str = re.findall(r"(Theory\s.*?)(?= Theory|$)", s_str, re.DOTALL)
+    if n is not None:
+        num_of_nodes = n + len(theories_str)
+        return grab_goals(s_str, num_of_nodes)
+    else:
+
+        theories = []
+        for theory in theories_str:
+            name_match = re.search(r"Theory\s+(.*),", theory)
+            if name_match:
+                name = name_match.group(1)
+                id_match = re.search(r"id:\s(\d+)", theory)
+                if id_match:
+                    id = id_match.group(1)
+                    goals = grab_goals(theory)
+                    theories.append(Theory(id, name, goals).__dict__)
+                else:
+                    raise RegexFailure("Failed to regex id in theory")
             else:
-                raise RegexFailure("Failed to regex id in theory")
-        else:
-            raise RegexFailure("Failed to regex name in theory")
+                raise RegexFailure("Failed to regex name in theory")
+        return theories
 
-def grab_files(s_str):
+def grab_files(s_str, n=None):
     names_match = re.findall(r"File\s(.*?)," , s_str)
-    # Check if names_match is empty, raise an error if it is
-    if not names_match:
-        raise RegexFailure("Failed to regex name in file, Check to see whether the regex is valid or the output changed")
-    ids_match = re.findall(r"id\s(\d+)" , s_str)
-    # Check if ids_match is empty, raise an error if it is
-    if not ids_match:
-        raise RegexFailure("Failed to regex id in file, Check to see whether the regex is valid or the output changed")
-    # Check if the length is equal
-    if len(names_match) != len(ids_match):
-        raise RegexFailure("Unequality in regex of id and names")
-    files = []
-    for name_match, id_match in zip(names_match, ids_match):
-        files.append(File(id_match, name_match, grab_theories(s_str)).__dict__)
-    return files
+    if n is not None:
+        num_of_nodes = len(names_match)
+        return grab_theories(s_str, num_of_nodes)
+
+    else:# Check if names_match is empty, raise an error if it is
+        if not names_match:
+            raise RegexFailure("Failed to regex name in file, Check to see whether the regex is valid or the output changed")
+        ids_match = re.findall(r"id\s(\d+)" , s_str)
+        # Check if ids_match is empty, raise an error if it is
+        if not ids_match:
+            raise RegexFailure("Failed to regex id in file, Check to see whether the regex is valid or the output changed")
+        # Check if the length is equal
+        if len(names_match) != len(ids_match):
+            raise RegexFailure("Unequality in regex of id and names")
+        files = []
+        for name_match, id_match in zip(names_match, ids_match):
+            files.append(File(id_match, name_match, grab_theories(s_str)).__dict__)
+        return files
 
 def grab_session(s_str):
     sess_match = re.search(r"^(\S+)", s_str)
@@ -174,6 +187,11 @@ def grab_session(s_str):
     else:
         raise RegexFailure("Failed to grab session")
 
+#def sel_node_num = 0
+#def next_node():
+#    sel_node_num++;
+#    return "On node: " node_num
+
 def grab_data(s_str):
     regex_type = vim.eval("s:regex_type") 
     match regex_type:
@@ -184,12 +202,22 @@ def grab_data(s_str):
                 raise FailedToGetData(f"Error regexing data for print: {e}") from e
             except Exception as e:
                 raise e
+        case "ng":
+            try:
+               return grab_selected_goal(s_str) 
+            except Exception as e:
+                raise e
         case "start": 
-            return {'start': 'server'}
+            return {'start': "session" }
         case "quit":
             return {'quit': 'server'}
+        case "initialize":
+            num_of_nodes = grab_files(s_str, 0)
+            s = "initialized with x nodes: ", num_of_nodes
+            return { 'server': s }
         case _:
             raise UnavailableCommand("Command is not available")
+
 
 def On_Ev(s_str):
     try:
@@ -199,6 +227,6 @@ def On_Ev(s_str):
         print(f"COMMAND ERROR: {e}")
     except FailedToGetData as e:
         print(f"REQ ERROR: {e}")
-    except:
-        print("Anomalous error")
+    except Exception as e:
+        print(f"Anomalous error: {e}")
     
